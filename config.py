672872f3dd3e0fde18e058fc1dd8338039c51f4e
zhengmeisong <zhengmeisong@dlgpu2.ai.bjcc.qihoo.net>
2019-07-28 13:58:03 +0800

    dl2

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ configurations = {
 
         BACKBONE_NAME = 'IR_50', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         HEAD_NAME = 'ArcFace', # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
-        LOSS_NAME = 'Softmax', # support: ['Focal', 'Softmax']
+        LOSS_NAME = 'Focal', # support: ['Focal', 'Softmax']
 
         INPUT_SIZE = [112, 112], # support: [112, 112] and [224, 224]
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
@@ -20,16 +20,16 @@ configurations = {
         EMBEDDING_SIZE = 512, # feature dimension
         BATCH_SIZE = 512,
         DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
-        LR = 0.01, # initial LR
-        NUM_EPOCH = 125, # total epoch number (use the firt 1/25 epochs to warm up)
+        LR = 0.1, # initial LR
+        NUM_EPOCH = 35, # total epoch number (use the firt 1/25 epochs to warm up)
         WEIGHT_DECAY = 5e-4, # do not apply to batch_norm parameters
         MOMENTUM = 0.9,
-        STAGES = [5, 10, 15, 25], # epoch stages to decay learning rate
+        STAGES = [3, 10, 15, 25], # epoch stages to decay learning rate
 
         DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
         MULTI_GPU = True, # flag to use multiple GPUs; 
         GPU_ID = [0, 1, 2, 3, 4, 5, 6, 7], # specify your GPU ids
         PIN_MEMORY = True,
-        NUM_WORKERS = 0,
+        NUM_WORKERS = 30,
 ),
 }
