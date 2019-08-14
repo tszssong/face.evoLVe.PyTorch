9156b0e0a06ff0989f7eff33b5a809d96816186e
tszssong <zhengmeisong@126.com>
2019-08-14 21:58:37 +0800

    update

--- imgdata/tri_hard_iter.py
+++ imgdata/tri_hard_iter.py
@@ -13,9 +13,10 @@ sys.path.append( os.path.join( os.path.dirname(__file__),'../imgdata/') )
 from model_resnet import ResNet_50, ResNet_101, ResNet_152
 from show_img import showBatch
 import multiprocessing
-from multiprocessing import Process, Manager, Lock
-# muxlock = multiprocessing.Lock()
-
+from multiprocessing import Lock, Process
+import resource
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
 def pil_loader(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
@@ -24,13 +25,17 @@ def jpeg4py_loader(path):
     with open(path, 'rb') as f:
         img = jpeg.JPEG(f).decode()
         return Image.fromarray(img)  #TODO: torchvison transpose use PIL img
-
+def myProcess(Process):
+    def run(self):
+        print("this is process {}".format(os.getpid()))
+    def __del__(self):
+        print("del the process {}".format(os.getpid()))
 
 class TripletHardImgData(data.Dataset):
     def __init__(self, root, model, \
                  batch_size, bag_size, input_size, \
                  transform=None, target_transform=None, \
-                 number_workers = 3, use_list = True):
+                 number_workers = 4, use_list = True):
         super(TripletHardImgData, self).__init__()
         print("triplet hard image dataloader inited")
         self.root = root
@@ -43,13 +48,15 @@ class TripletHardImgData(data.Dataset):
         self._cur = 0
         self.bag_img_seq = []
         self.bag_lab_seq = []
+        self.bag_data = []
+        self.bag_target = []
         self.n_workers = number_workers
         self._inited = False
         if use_list:
             samples = self._read_paths(self.root)
         else:
-            print("not support now!")
-        # random.shuffle(samples)
+            print("no longer support now!")
+        
         self.samples = samples                #samples is a list like below:
         start = time.time()
         # random.shuffle(self.samples)
@@ -57,8 +64,6 @@ class TripletHardImgData(data.Dataset):
         self.targets = [s[1] for s in samples] # targets is a list with ids
         # print( "shuffle %d samples used time: %.2f s"%(len(self.samples), time.time()-start) )
         sys.stdout.flush()
-        # self.reset(self.model)
-
 
     def _read_paths(self, path):
         images = []
@@ -77,6 +82,7 @@ class TripletHardImgData(data.Dataset):
             return len(self.bag_img_seq)
         else:
             return len(self.samples)
+
     #https://blog.csdn.net/Tan_HandSome/article/details/82501902
     def _get_dist(self, emb):
         vecProd = np.dot(emb, emb.transpose())
@@ -92,12 +98,11 @@ class TripletHardImgData(data.Dataset):
         ed = np.sqrt(sq_ed)
 
         return np.asarray(ed)
-    # def _load_img   
-
+    
     def _get_bag(self):
-        bagdata = torch.empty(self.bag_size, 3, self.input_size[0], self.input_size[1])
-        baglabel = torch.empty(self.bag_size, 1)
-        index = 0
+        
+        del self.bag_data[:]
+        del self.bag_target[:]  
         if self._cur + self.bag_size > len(self.samples):
             self._cur = 0    # not enough for a bag
             # random.shuffle(self.samples)
@@ -108,67 +113,73 @@ class TripletHardImgData(data.Dataset):
         for idx in range(self.bag_size):
             q_in[idx%len(q_in)].append(bag_samples[idx])
         
-        with multiprocessing.Manager() as MG:
-            p_img = [ multiprocessing.Manager().list() for i in range(self.n_workers) ]
-            p_lab = [ multiprocessing.Manager().list() for i in range(self.n_workers) ]
-            loaders = [ multiprocessing.Process( target =self._load_func, \
-                args=(q_in[i], p_img[i], p_lab[i] ) ) for i in range(self.n_workers) ]
+        # with multiprocessing.Manager() as MG:
+        p_img = [ multiprocessing.Manager().list() for i in range(self.n_workers) ]
+        p_lab = [ multiprocessing.Manager().list() for i in range(self.n_workers) ]
+        loaders = [ Process( target =self._load_func, \
+            args=(q_in[i], p_img[i], p_lab[i] ) ) for i in range(self.n_workers) ]
         
-        lock = Lock()
         for p in loaders:
             p.start()
         for p in loaders:
             p.join() 
-        l_img = []
-        l_label = [] 
+            
         for imgs in p_img:
-            # print(len(img))
-            l_img.extend(imgs)
+            self.bag_data.extend(imgs)
         for labels in p_lab:
-            l_label.extend(labels) 
-        for idx in range(len(l_img)):
-            bagdata[idx] = l_img[idx]
-            baglabel[idx] = l_label[idx]       
-        return bagdata, baglabel
-        # return
+            self.bag_target.extend(labels)
+        del p_img[:]
+        del p_lab[:]     
+
     def _load_func(self, qin, pimg, plabel):  
         for item in qin:
             path, target = item 
-            img = pil_loader(path)
-            if self.transform is not None:
-                img = self.transform(img)
+            # img = pil_loader(path)  
+            # img = np.asarray(img)
+            img = cv2.imread(path) 
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)     
+            # if self.transform is not None:
+            #     img = self.transform(img)
+            img = img.transpose((2,0,1))
+            img = img.astype(np.float32)
+            img -=128
+            img /= 127.5
+            img = torch.from_numpy(img)
+            # img = torch.empty(3,112,112)
+            # print(os.getpid(), img.size(), img.dtype)
             pimg.append(img)
-            plabel.append(target)
-        
+            plabel.append(int(target) )
+
     def reset(self, model=None, device="cpu"):
         self._inited = True
         model.eval()
         model.to(device)
         start = time.time()
-        self.bag_img_seq = []
-        self.bag_lab_seq = []
-        bagdata, baglabel = self._get_bag()
+        self._get_bag()
         # if(np.where(baglabel==baglabel[0])[0].size == baglabel.size(0))
         print("loadImg time: %.6f s"%((time.time()-start)), end=' ')    
         start = time.time()
         # features = torch.empty(self.bag_size, self.embedding_size)
         features = torch.empty(self.bag_size, 512)
         for idx in range(int(self.bag_size/self.batch_size)):
-            fea = model(bagdata[idx*self.batch_size:(idx+1)*self.batch_size,:].to(device))
+            batch_img = self.bag_data[idx*self.batch_size:(idx+1)*self.batch_size]
+            batch_tensor = torch.empty(self.batch_size, 3, 112, 112)
+            for i in range(self.batch_size):
+                batch_tensor[i] = batch_img[i]
+            fea = model(batch_tensor.to(device))
             fea = F.normalize(fea).detach()
             features[ idx*self.batch_size:(idx+1)*self.batch_size,: ] = fea
         features.cpu()
         print("getFeature time: %.6f s"%((time.time()-start))) 
-        # features = model( bagdata.to(device) )
-        # features = F.normalize(features).detach().cpu()
         start = time.time()
         dist_matrix = torch.empty(self.bag_size, self.bag_size)
         dist_matrix = self._get_dist(features.numpy())
         np.set_printoptions(suppress=True)
-        print("dataLoader reset:", bagdata.size(), "distMatric use time: %.6f s"%((time.time()-start)))
+        print("dataLoader reset:", len(self.bag_data), "distMatric use time: %.6f s"%((time.time()-start)))
         sys.stdout.flush()
         assert dist_matrix.shape[0] == self.bag_size
-        baglabel_1v = baglabel.view(baglabel.shape[0]).numpy().astype(np.int32)
+        # baglabel_1v = self.bag_target.view(self.bag_target.shape[0]).numpy().astype(np.int32)
+        baglabel_1v = np.array(self.bag_target, dtype=np.int32)
         for a_idx in range( self.bag_size ):
             p_dist = dist_matrix[a_idx].copy()
             n_dist = dist_matrix[a_idx]
@@ -189,21 +200,21 @@ class TripletHardImgData(data.Dataset):
             n_idx = np.random.choice(n_candidate)
             # print("dist after:", n_dist)
             # print("triplet find:", a_idx, p_idx, n_idx)
-            self.bag_img_seq.append((bagdata[a_idx],bagdata[p_idx],bagdata[n_idx]))
+            self.bag_img_seq.append((self.bag_data[a_idx],self.bag_data[p_idx],self.bag_data[n_idx]))
             self.bag_lab_seq.append( (int( baglabel_1v[a_idx] ), \
                                       int( baglabel_1v[p_idx] ), \
                                       int( baglabel_1v[n_idx] ) ) ) 
-          
-
+                  
 
 if __name__=='__main__':
+    print("main process {}".format(os.getpid()))
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     parser = argparse.ArgumentParser(description='triplet image iter')
-    parser.add_argument('--data-root', type=str, default='/home/ubuntu/zms/data/ms1m_emore100/')
+    parser.add_argument('--data-root', type=str, default='/home/ubuntu/zms/data/ms1m_emore_img/')
     parser.add_argument('--batch-size', type=int, default=12)
     parser.add_argument('--image-size', default=[112, 112])
     parser.add_argument('--model-path', type=str, default='/home/ubuntu/zms/models/ResNet_50_Epoch_33.pth')
-    parser.add_argument('--bag-size', type=int, default=12)
+    parser.add_argument('--bag-size', type=int, default=120)
     args = parser.parse_args()
     im_width = args.image_size[0]
     im_heigh = args.image_size[1]
