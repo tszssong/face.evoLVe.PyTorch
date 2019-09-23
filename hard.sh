0d431ebf25fa9f624806c531cc0aea58338e0486
zhengmeisong <zhengmeisong@test04.pm.corp.qihoo.net>
2019-09-23 09:38:04 +0800

    V2.0

--- hard.sh
+++ hard.sh
@@ -4,4 +4,4 @@ python tri_hard_train.py --data-root /data3/zhengmeisong/data/gl2ms1m_dl23W1f1_1
   --lr 0.01 --lr-stages 6000,12000,18000,24000 --margin 0.5 \
   --backbone-resume-root ../py-model/ResNet_50_Epoch_35.pth \
   --gpu-ids 0,1,2,3 \
-  2>&1 | tee ../py-log/tri_hard_`date +'%m_%d-%H_%M'`.log
+  2>&1 | tee ../py-log/tri_hard_`date +'%m_%d-%H_%M'`.log  
