baa7dbb2cdc1ec04d12977e70808559d1132f82c
zhengmeisong <zhengmeisong@olgpu11.ai.shbt.qihoo.net>
2019-11-01 07:53:56 +0800

    resnet-attention

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ configurations = {
     1: dict(
         SEED = 1337, # random seed for reproduce results
 
-        DATA_ROOT = '/data3/zhengmeisong/data/ms1m_ali111', # the parent root where your train/val/test data are stored
+        DATA_ROOT = '/data01/zhengmeisong/data/ms1m-retina-1w/', # the parent root where your train/val/test data are stored
         MODEL_ROOT = '../py-model', # the root to buffer your checkpoints
         LOG_ROOT = '../py-log', # the root to log your train/val status
         BACKBONE_RESUME_ROOT = '../py-model/backbone_ir50_ms1m_epoch120.pth', # the root to resume training from a saved checkpoint
@@ -12,26 +12,26 @@ configurations = {
 
         BACKBONE_NAME = 'ResNet_50', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         HEAD_NAME = 'ArcFace',       # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
-        LOSS_NAME = 'Triplet',   # support: ['Focal', 'Softmax', 'Triplet']
+        LOSS_NAME = 'Focal',   # support: ['Focal', 'Softmax', 'Triplet']
 
         INPUT_SIZE = [112, 112], # support: [112, 112] and [224, 224]
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
         RGB_STD = [0.5, 0.5, 0.5],
         EMBEDDING_SIZE = 512, # feature dimension
-        BATCH_SIZE = 40,
+        BATCH_SIZE = 64,
         DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
         LR = 0.1, # initial LR
         NUM_EPOCH = 12500, # total epoch number (use the firt 1/25 epochs to warm up)
         WEIGHT_DECAY = 5e-4, # do not apply to batch_norm parameters
         MOMENTUM = 0.9,
-        STAGES = [135, 465, 895], # epoch stages to decay learning rate
+        STAGES = [5,10,15,25], # epoch stages to decay learning rate
         #STAGES = [35, 65, 95], # epoch stages to decay learning rate
 
-        DEVICE = torch.device("cuda:3" if torch.cuda.is_available() else "cpu"),
+        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
         MULTI_GPU = True, # flag to use multiple GPUs; 
-        GPU_ID = [3], # specify your GPU ids
+        GPU_ID = [0,1,2,3], # specify your GPU ids
         PIN_MEMORY = True,
-        NUM_WORKERS = 16,
+        NUM_WORKERS = 4,
     ),
 }
 
