b56f62129a06add2f5ac47d25e5923536b2dc382
zhengmeisong <zhengmeisong@test04.pm.corp.qihoo.net>
2019-11-03 17:39:24 +0800

    update

--- hardIR.sh
+++ hardIR.sh
@@ -1,8 +1,8 @@
-  #--backbone-resume-root ../py-model/Backbone_ResNet_50_Epoch_1_Batch_9801_Time_2019-09-27-13-03_checkpoint.pth \
+#python tri_hard_train.py --data-root /data3/zhengmeisong/lmdbTrain/gl2ms1m_dl23W1f1_150WW1/ \
 python tri_hard_train.py --data-root /data3/zhengmeisong/data/gl2ms1m_dl23W1f1_150WW1_img/ \
-  --num-workers 4 --num-epoch 100 \
-  --bag-size 48000 --batch-size 480 --test-freq 50 --save-freq 50\
-  --lr 0.005 --lr-stages 120000 --margin 0.5 \
+  --num-workers 0 --num-epoch 100 \
+  --bag-size 72000 --batch-size 480 --test-freq 50 --save-freq 50\
+  --lr 0.01 --lr-stages 200,400,1800 --margin 0.5 \
   --backbone-name IR_50 \
   --backbone-resume-root ../py-model/IR50_ms1m_epoch120.pth \
   --gpu-ids 0,1,2,3 \
