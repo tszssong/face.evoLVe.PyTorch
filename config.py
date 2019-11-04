4164f416a5e9d7f7ec270cc82c39a5255b17b703
zhengmeisong <zhengmeisong@olgpu6.ai.shbt.qihoo.net>
2019-11-04 17:41:21 +0800

    resnet attention

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ configurations = {
         SEED = 1337, # random seed for reproduce results
 
         # DATA_ROOT = '/cloud_data01/zhengmeisong/data/ms1m_emore_img/', # the parent root where your train/val/test data are stored
-        DATA_ROOT = '/cloud_data01/zhengmeisong/data/ol11/ms1m-retinaface-t1-img/',
+        DATA_ROOT = '/home/zhengmeisong/data/ms1m-retinaface-t1-img/',
         MODEL_ROOT = '../py-model', # the root to buffer your checkpoints
         LOG_ROOT = '../py-log', # the root to log your train/val status
         BACKBONE_RESUME_ROOT = '../py-model/backbone_ir50_ms1m_epoch120.pth', # the root to resume training from a saved checkpoint
@@ -20,19 +20,20 @@ configurations = {
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
         RGB_STD = [0.5, 0.5, 0.5],
         EMBEDDING_SIZE = 512, # feature dimension
-        BATCH_SIZE = 512,
+        BATCH_SIZE = 480,
         DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
-        LR = 0.01, # initial LR
-        NUM_EPOCH = 12, # total epoch number (use the firt 1/25 epochs to warm up)
+        LR = 0.1, # initial LR
+        NUM_EPOCH = 15, # total epoch number (use the firt 1/25 epochs to warm up)
         WEIGHT_DECAY = 5e-4, # do not apply to batch_norm parameters
         MOMENTUM = 0.9,
         # STAGES = [5,10,15,25], # epoch stages to decay learning rate
-        STAGES = [3, 6, 9], # epoch stages to decay learning rate
+        STAGES = [3, 6, 9, 12], # epoch stages to decay learning rate
 
-        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
         MULTI_GPU = True, # flag to use multiple GPUs; 
+        # DEVICE = torch.device("cuda:4" if torch.cuda.is_available() else "cpu"),
+        # GPU_ID = [4,5,6,7], # specify your GPU ids
+        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
         GPU_ID = [0,1,2,3,4,5,6,7], # specify your GPU ids
-        # GPU_ID = [0,1,2,3], # specify your GPU ids
         PIN_MEMORY = True,
         NUM_WORKERS = 0,
     ),
