b620fc26edc2f0e1ffdeacf4cf30fae3b7097245
zhengmeisong <zhengmeisong@hpcgpu37.ai.zzzc.qihoo.net>
2019-11-06 16:08:31 +0800

    r18

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ configurations = {
         SEED = 1337, # random seed for reproduce results
 
         # DATA_ROOT = '/cloud_data01/zhengmeisong/data/ms1m_emore_img/', # the parent root where your train/val/test data are stored
-        DATA_ROOT = '/home/zhengmeisong/data/ms1m-retinaface-t1-img/',
+        DATA_ROOT = '/data03/zhengmeisong/data/ms1m_emore_img/',
         MODEL_ROOT = '../py-model', # the root to buffer your checkpoints
         LOG_ROOT = '../py-log', # the root to log your train/val status
-        BACKBONE_RESUME_ROOT = '../py-model/backbone_ir50_ms1m_epoch120.pth', # the root to resume training from a saved checkpoint
+        BACKBONE_RESUME_ROOT = './py-model/backbone_ir50_ms1m_epoch120.pth', # the root to resume training from a saved checkpoint
         HEAD_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
 
-        BACKBONE_NAME = 'RA_92', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
+        BACKBONE_NAME = 'IR_18', # support: ['IR_18','ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         # BACKBONE_NAME = 'IR_SE_50', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         HEAD_NAME = 'Softmax',       # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
         LOSS_NAME = 'Softmax',   # support: ['Focal', 'Softmax', 'Triplet']
@@ -20,7 +20,7 @@ configurations = {
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
         RGB_STD = [0.5, 0.5, 0.5],
         EMBEDDING_SIZE = 512, # feature dimension
-        BATCH_SIZE = 480,
+        BATCH_SIZE = 512,
         DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
         LR = 0.1, # initial LR
         NUM_EPOCH = 15, # total epoch number (use the firt 1/25 epochs to warm up)
@@ -32,8 +32,8 @@ configurations = {
         MULTI_GPU = True, # flag to use multiple GPUs; 
         # DEVICE = torch.device("cuda:4" if torch.cuda.is_available() else "cpu"),
         # GPU_ID = [4,5,6,7], # specify your GPU ids
-        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
-        GPU_ID = [0,1,2,3,4,5,6,7], # specify your GPU ids
+        DEVICE = torch.device("cuda:2" if torch.cuda.is_available() else "cpu"),
+        GPU_ID = [2,3], # specify your GPU ids
         PIN_MEMORY = True,
         NUM_WORKERS = 0,
     ),
