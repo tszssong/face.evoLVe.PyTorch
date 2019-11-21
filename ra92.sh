f5d61c3f33378e4dd329b173c43dabe7e8b765a3
zhengmeisong <zhengmeisong@hpcgpu38.ai.zzzc.qihoo.net>
2019-11-21 16:47:32 +0800

    ra92

---
+++ ra92.sh
@@ -0,0 +1,9 @@
+export OMP_NUM_THREADS=4
+MODEL=RA_92
+#python myTrain.py  --backbone-name $MODEL --data-root /cloud_data01/zhengmeisong/data/gl2ms1m_imgs/ \
+python myTrain.py  --backbone-name ${MODEL} --data-root /home/zhengmeisong/hpc37/gl2ms1m_img/ \
+  --batch-size 160 \
+  --lr 0.1 --lr-stages 8,12,16,18 --num-epoch 25 \
+  --loss-name Softmax --head-name Combine \
+  --model-root ../py-model/${MODEL}/ \
+  --gpu-ids 0,1 2>&1 | tee ../py-log/ra92Combine_`date +'%m_%d-%H_%M'`.log
