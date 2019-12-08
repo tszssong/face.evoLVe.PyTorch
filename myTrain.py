d4d8744e9d4005543fbb225df5b659f7983a28d4
zhengmeisong <zhengmeisong@hpcgpu14.ai.lycc.qihoo.net>
2019-12-08 14:01:36 +0800

    torch transpose

--- myTrain.py
+++ myTrain.py
@@ -15,7 +15,8 @@ from head.metrics import ArcFace, CosFace, SphereFace, Am_softmax, Softmax,Combi
 from loss.loss import FocalLoss, TripletLoss
 from util.utils import make_weights_for_balanced_classes, get_val_data, get_val_pair, separate_irse_bn_paras, separate_resnet_bn_paras, warm_up_lr, schedule_lr, perform_val, get_time, buffer_val, AverageMeter, accuracy
 from tqdm import tqdm
-
+from imgdata.show_img import showBatch, saveBatch
+import torch.nn.functional as F
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -61,12 +62,16 @@ if __name__ == '__main__':
     train_transform = transforms.Compose([ 
         # transforms.Resize([int(128 * INPUT_SIZE[0] / 112), int(128 * INPUT_SIZE[0] / 112)]), # smaller side resized
         # transforms.RandomCrop([INPUT_SIZE[0], INPUT_SIZE[1]]),
+        transforms.ColorJitter(brightness=1),
+        # transforms.ColorJitter(contrast=1),
+        # transforms.ColorJitter(saturation=0.5),
+        # transforms.ColorJitter(hue=0.5),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean =  [0.5, 0.5, 0.5], std =  [0.5, 0.5, 0.5]),
     ])
 
-    dataset_train = datasets.ImageFolder(os.path.join(args.data_root, 'imgs'), train_transform)
+    dataset_train = datasets.ImageFolder(os.path.join(args.data_root, 'data_100'), train_transform)
 
     # create a weighted random sampler to process imbalanced data
     # weights = make_weights_for_balanced_classes(dataset_train.imgs, len(dataset_train.classes))
@@ -157,6 +162,11 @@ if __name__ == '__main__':
             labels = labels.to(DEVICE).long()
             features = BACKBONE(inputs)
             outputs = HEAD(features, labels)
+            ##save img samples##################
+            bFeature = F.normalize(outputs).detach()
+            saveBatch(inputs.cpu().numpy(), labels.cpu().numpy(), \
+                bFeature.cpu().numpy(), show_x=10, batchIdx=batch)
+            #####################
             loss = LOSS(outputs, labels)
             print('loss:',loss.cpu().detach().numpy())
             # measure accuracy and record loss
@@ -198,14 +208,14 @@ if __name__ == '__main__':
 
         # perform validation & save checkpoints per epoch
         # # validation statistics per epoch (buffer for visualization)
-        print("=" * 60)
-        print("Perform Evaluation on LFW, CFP_FF, CFP_FP, AgeDB, CALFW, CPLFW and VGG2_FP, and Save Checkpoints...")
-        accuracy_lfw, best_threshold_lfw = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, lfw, lfw_issame)
-        accuracy_cfp_fp, best_threshold_cfp_fp = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, cfp_fp, cfp_fp_issame)
-        accuracy_agedb, best_threshold_agedb = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, agedb, agedb_issame)
-        print("Epoch {}/{}, Evaluation: LFW Acc: {}, CFP_FP Acc: {}, AgeDB Acc: {}".format(epoch + 1, args.num_epoch, accuracy_lfw, accuracy_cfp_fp, accuracy_agedb))
-        print("=" * 60)
-        sys.stdout.flush() 
+        # print("=" * 60)
+        # print("Perform Evaluation on LFW, CFP_FF, CFP_FP, AgeDB, CALFW, CPLFW and VGG2_FP, and Save Checkpoints...")
+        # accuracy_lfw, best_threshold_lfw = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, lfw, lfw_issame)
+        # accuracy_cfp_fp, best_threshold_cfp_fp = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, cfp_fp, cfp_fp_issame)
+        # accuracy_agedb, best_threshold_agedb = perform_val(MULTI_GPU, DEVICE, args.emb_size, args.batch_size, BACKBONE, agedb, agedb_issame)
+        # print("Epoch {}/{}, Evaluation: LFW Acc: {}, CFP_FP Acc: {}, AgeDB Acc: {}".format(epoch + 1, args.num_epoch, accuracy_lfw, accuracy_cfp_fp, accuracy_agedb))
+        # print("=" * 60)
+        # sys.stdout.flush() 
         # save checkpoints per epoch
         if MULTI_GPU:
             torch.save(BACKBONE.module.state_dict(), os.path.join(args.model_root, "Backbone_{}_Epoch_{}_Batch_{}_Time_{}_checkpoint.pth".format(args.backbone_name, epoch + 1, batch, get_time())))
--- ra92.sh
+++ ra92.sh
@@ -1,12 +1,13 @@
-export OMP_NUM_THREADS=4
+#export OMP_NUM_THREADS=4
+  #--head-resume-root /data_luoqi/zhengmeisong/wkspace/py-model//RA_92/Head_Combine_Epoch_6_Batch_160368_Time_2019-12-07-15-29_checkpoint.pth \
+	  #python myTrainDali.py  --backbone-name ${MODEL} --data-root /data_luoqi/zhengmeisong/data/gl2ms1m_img/ \
 MODEL=RA_92
 mkdir ../py-model/${MODEL}/
-python myTrainDali.py  --backbone-name ${MODEL} --data-root /mnt/sdc/zhengmeisong/data/glintv2_emore_ms1m_img/ \
-  --backbone-resume-root ../py-preTrain/Backbone_RA_92_Epoch_1_Batch_26728_Time_2019-12-03-23-01_checkpoint.pth \
-  --head-resume-root ../py-preTrain/Head_Combine_Epoch_1_Batch_26728_Time_2019-12-03-23-01_checkpoint.pth \
-  --batch-size 64 --num-workers 2 \
+python myTrain.py  --backbone-name ${MODEL} --data-root /data_luoqi/zhengmeisong/data/gl2ms1m_img/ \
+  --backbone-resume-root /data_luoqi/zhengmeisong/wkspace/py-model//RA_92/Backbone_RA_92_Epoch_6_Batch_160368_Time_2019-12-07-15-29_checkpoint.pth \
+  --batch-size 128 --num-workers 4 \
   --lr 0.0001 --lr-stages 4,8,12 --num-epoch 15 \
   --loss-name Softmax --head-name Combine \
   --model-root ../py-model/${MODEL}/ \
-  --gpu-ids 0 2>&1 | tee ../py-log/ra92Combine_`date +'%m_%d-%H_%M'`.log
+  --gpu-ids 2,3 2>&1 | tee ../py-log/ra92Combine_`date +'%m_%d-%H_%M'`.log
 
